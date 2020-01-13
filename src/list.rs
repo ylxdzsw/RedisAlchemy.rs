7a88d38d65c6a0377b4ebc031973b420e97dd6b0
Shiwei Zhang <ylxdzsw@gmail.com>
2020-01-13 17:32:15 +0800

    iterator for list

--- src/list.rs
+++ src/list.rs
@@ -1,5 +1,6 @@
 use crate::*;
 use std::borrow::Borrow;
+use std::collections::VecDeque;
 
 /// List is conceptually similar to Vec<T>
 pub struct List<A, K, T=Box<[u8]>>
@@ -9,7 +10,15 @@ pub struct List<A, K, T=Box<[u8]>>
     phantom: std::marker::PhantomData<T>
 }
 
-impl<A, K: Borrow<[u8]>, T: serde::Serialize + serde::Deserialize<'static>> List<A, K, T> where for<'a> &'a A: AsRedis {
+fn serialization_error<T>(_: T) -> RedisError {
+    RedisError::OtherError("Serialization failed".to_string())
+}
+
+fn deserialization_error<T>(_: T) -> RedisError {
+    RedisError::OtherError("Deserialization failed".to_string())
+}
+
+impl<A, K: Borrow<[u8]>, T: serde::Serialize + serde::de::DeserializeOwned> List<A, K, T> where for<'a> &'a A: AsRedis {
     pub fn new(client: A, key: K) -> Self {
         Self { client, key, phantom: std::marker::PhantomData }
     }
@@ -23,7 +32,55 @@ impl<A, K: Borrow<[u8]>, T: serde::Serialize + serde::Deserialize<'static>> List
     }
 
     pub fn push(&self, x: &T) -> Result<(), RedisError> {
-        let buf = serde_json::to_vec(x).map_err(|_| RedisError::OtherError("Serialization failed".to_string()))?;
+        let buf = serde_json::to_vec(x).map_err(serialization_error)?;
         self.initiate(b"rpush").arg(&buf).fetch().map(|x| x.ignore())
+    }
+
+    pub fn get(&self, i: usize) -> Result<T, RedisError> {
+        let buf = self.initiate(b"lindex").arg(i.to_string().as_bytes()).fetch().map(|x| x.bytes())?;
+        let res: T = serde_json::from_slice(&buf).map_err(deserialization_error)?;
+        Ok(res)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item=T> + '_ {
+        self.into_iter()
+    }
+}
+
+const BATCH_SIZE: usize = 12;
+
+pub struct ListIter<'l, A, K, T> {
+    buf: VecDeque<T>,
+    index: usize,
+    list: &'l List<A, K, T>
+}
+
+impl<'l, A, K: Borrow<[u8]>, T: serde::Serialize + serde::de::DeserializeOwned> Iterator for ListIter<'l, A, K, T> where for<'a> &'a A: AsRedis {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.buf.is_empty() { // try to get a batch
+            let batch = self.list.initiate(b"lrange")
+                .arg(self.index.to_string().as_bytes())
+                .arg((self.index + BATCH_SIZE).to_string().as_bytes())
+                .fetch().expect("Error during iteration").list();
+
+            self.index += batch.len();
+            for x in batch.into_iter() {
+                let x = serde_json::from_slice(&x.bytes()).map_err(deserialization_error).expect("Error during iteration");
+                self.buf.push_back(x)
+            }
+        }
+
+        self.buf.pop_front()
+    }
+}
+
+impl<'l, A, K: Borrow<[u8]>, T: serde::Serialize + serde::de::DeserializeOwned> IntoIterator for &'l List<A, K, T> where for<'a> &'a A: AsRedis {
+    type Item = T;
+    type IntoIter = ListIter<'l, A, K, T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ListIter { buf: VecDeque::with_capacity(BATCH_SIZE), index: 0, list: self }
     }
 }
--- tests/list.rs
+++ tests/list.rs
@@ -1,8 +1,11 @@
 use redis_alchemy::*;
 
 #[test]
-fn blob() {
+fn list_iter() {
     let client = TcpClient::new("127.0.0.1:6379");
-    let list: List<_, _, i32> = List::new(client, &b"list"[..]);
+    let list: List<_, _, i32> = List::new(client, &b"list_iter"[..]);
     list.push(&-24).unwrap();
+    list.push(&257).unwrap();
+    let x: Vec<_> = list.iter().collect();
+    assert_eq!(&x, &[-24, 257])
 }
